32a368d8d3740e36fd26d87385a0e82db7590319
DonCatalin <catalindontu16@gmail.com>
2024-10-26 20:11:53 +0300

    initial commit

---
+++ src/main/java/com/example/actionwithspring/ActionWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.actionwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actionwithspring/ActionWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actionwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
